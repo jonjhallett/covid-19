3a712f081168bb5ba6701000a7858a00fab1f983
Jon Hallett <jon.hallett@bristol.ac.uk>
2020-03-13 11:01:36 +0000

    Hoist more constants

--- prob.py
+++ prob.py
@@ -2,25 +2,28 @@
 
 import math
 
-population_of_uk = 66400000
 deaths_in_uk = 10
+
+population_of_uk = 66400000
+fatality_rate = 0.009
+doubling_rate_in_days = 6.2
+average_time_to_death_in_days = 17.3
+
 
 def main():
     for number_of_people in range(350, 1351, 100):
         probability = probability_of_at_least_one_case(number_of_people)
         print(f'{number_of_people}\t{probability:0.2f}')
 
-def deaths_to_true_cases(number_of_deaths):
-    fatality_rate = 0.009
-    doubling_rate_in_days = 6.2
-    average_time_to_death_in_days = 17.3
 
+def deaths_to_true_cases(number_of_deaths):
     cases_to_deaths_factor = 1 / fatality_rate
     time_from_initial_infection_to_death_factor = 2 ** (average_time_to_death_in_days / doubling_rate_in_days)
 
     true_number_of_cases = number_of_deaths * cases_to_deaths_factor * time_from_initial_infection_to_death_factor
 
     return true_number_of_cases
+
 
 def probability_of_at_least_one_case(number_of_people):
     true_cases_in_uk = deaths_to_true_cases(deaths_in_uk)
@@ -31,5 +34,6 @@ def probability_of_at_least_one_case(number_of_people):
 
     probability_of_at_least_one_case = 1 - probability_of_no_cases
     return probability_of_at_least_one_case
+
 
 main()
